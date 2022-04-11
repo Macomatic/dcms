c25eefac3b3a3db9bf5fc34c0250eaa29d10b666
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-04-11 15:34:41 -0400

    Format and style of addPatient finished
    No more sql errors pop on the site,  mandatory fields are now required prior to submission, format for confirmation/error messages now fixed

--- receptionistAddPatient.php
+++ receptionistAddPatient.php
@@ -3,12 +3,13 @@
 require_once "config.php";
 
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-  $fullName = $_POST['fName'].$_POST['mName'].' '.$_POST['lName'];
-  $fullAddress = $_POST['st'].', '.$_POST['city'].', '.$_POST['prov'];
-  $randomNum = random_int(1,99999999);
-  $userName = $fullName.$randomNum;
-  $password = "password";
-  $role = "patient";
+  if ($_POST['fName'] != NULL && $_POST['lName'] != NULL && $_POST['st'] != NULL && $_POST['city'] != NULL && $_POST['prov'] != NULL && $_POST['dob'] != NULL && $_POST['email'] != NULL && $_POST['phoneNum'] != NULL && $_POST['lName'] != NULL && $_POST['gender'] != NULL && $_POST['insurance'] != NULL && $_POST['ssn'] != NULL) {
+    $fullName = $_POST['fName'].$_POST['mName'].' '.$_POST['lName'];
+    $fullAddress = $_POST['st'].', '.$_POST['city'].', '.$_POST['prov'];
+    $randomNum = random_int(1,99999999);
+    $userName = $fullName.$randomNum;
+    $password = "password";
+    $role = "patient";
     $sql = "insert into dcms.\"User\"(user_ID,username,password,role) values('".$randomNum."','".$userName."','".md5($password)."','".$role."')";
     $ret = pg_query($dbconnect, $sql);
     if($ret) {
@@ -16,9 +17,8 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
       $sql = "insert into dcms.Patient(patient_ID,name,gender,insurance,ssn,email,dateOfBirth,address) values('".$randomNum."','".$fullName."','".$_POST['gender']."','".$_POST['insurance']."','".$_POST['ssn']."','".$_POST['email']."','".$_POST['dob']."','".$fullAddress."')";
       $ret = pg_query($dbconnect, $sql);
       if($ret) {
-        echo "<p style='color:#39C16E;font-weight: bold;'>"."Data saved Successfully. Returning to main reception screen..."."</p>";
-        sleep(2);
-        header('Location: receptionist.php');
+        echo "<p style='color:#39C16E;font-weight: bold;'>".$_POST['fName']." was added to the database succesfully!"."</p>";
+        // /header('Location: receptionist.php');
       }
       else { 
         echo "Something Went Wrong";
@@ -28,8 +28,12 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     else { 
         echo "Something Went Wrong";
     }
+  
+  }
 
-    
+  else {
+    echo "<p style='color:#EA0730;font-weight: bold;'>"."Please fill out all required fields marked with an *"."</p>";
+  }
     
 }
 ?>
@@ -46,12 +50,14 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
 
 <div class="container">
   <h2>Add a new patient</h2>
+  <a href="receptionist.php">
+      <button>Go back</button>
+    </a>
   <form method="post">
   
-    
     <h3>Patient Name</h3>
     <div class="form-group">
-      <label for="fName">First Name:</label>
+      <label for="fName">First Name*:</label>
       <input type="text" class="form-control" id="fName" placeholder="Enter first name" name="fName">
     </div>
 
@@ -61,24 +67,24 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     </div>
 
     <div class="form-group">
-      <label for="lName">Last Name:</label>
+      <label for="lName">Last Name*:</label>
       <input type="text" class="form-control" id="lName" placeholder="Enter last name" name="lName">
     </div>
     
      <br>
     <h3>Address</h3>
     <div class="form-group">
-      <label for="st">Street # and name:</label>
+      <label for="st">Street # and name*:</label>
       <input type="text" class="form-control" id="st" placeholder="Enter street number and name" name="st">
     </div>
 
     <div class="form-group">
-      <label for="city">City:</label>
+      <label for="city">City*:</label>
       <input type="text" class="form-control" id="city" placeholder="Enter city" name="city">
     </div>
 
     <div class="form-group">
-      <label for="prov">Province:</label>
+      <label for="prov">Province*:</label>
       <input type="text" class="form-control" id="prov" placeholder="Enter province" name="prov">
     </div>
      
@@ -86,22 +92,22 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     <br>
     <h3>Personal Info</h3>
     <div class="form-group">
-      <label for="dob">Date of Birth:</label>
+      <label for="dob">Date of Birth*:</label>
       <input type="date" class="form-control" id="dob" placeholder="mm / dd / yyyy" name="dob">
     </div>
 
     <div class="form-group">
-      <label for="email">Email Address:</label>
+      <label for="email">Email Address*:</label>
       <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
 
     <div class="form-group">
-      <label for="phoneNum">Phone Number:</label>
+      <label for="phoneNum">Phone Number*:</label>
       <input type="number" class="form-control" id="phoneNum" placeholder="Enter phone number" name="phoneNum">
     </div>
 
     <div class="form-group">
-      <label for="gender">Gender:</label>
+      <label for="gender">Gender*:</label>
       <select name="gender" id="gender" class="form-control">
         <option value="male">Male</option>
         <option value="female">Female</option>
@@ -110,12 +116,12 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     </div>
 
     <div class="form-group">
-      <label for="insurance">Insurance:</label>
+      <label for="insurance">Insurance*:</label>
       <input type="text" class="form-control" id="insurance" placeholder="Enter insurance" name="insurance">
     </div>
 
     <div class="form-group">
-      <label for="ssn">SSN:</label>
+      <label for="ssn">SSN*:</label>
       <input type="number" class="form-control" id="ssn" placeholder="Enter SSN" name="ssn">
     </div>
 
