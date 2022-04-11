ac26eb15b7519fd9c0bc60f57942ad30256d98fc
Marco Vethanayagam <29697062+Macomatic@users.noreply.github.com>
2022-04-11 16:10:43 -0400

    Basic PostgreSQL pull functionality

--- receptionist.php
+++ receptionist.php
@@ -1,3 +1,18 @@
+<?php
+  // Include config file
+  require_once "config.php";
+  error_reporting(E_ALL ^ E_DEPRECATED);
+
+  if(isset($_POST['submit'])&&!empty($_POST['submit'])){
+    $query = 'select * from dcms."User"';
+    $rs = pg_query($query) or die ("Error: ".pg_last_error());
+    while ($row = pg_fetch_row($rs)) {
+      echo "$row[0] $row[1] $row[2] $row[3]\n";
+    }
+  }
+  
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,6 +31,12 @@
     </a>
     <label for="search">Search for a patient: </label>
     <input type="text" id="search" name="search" placeholder="Search for a name">
+</div>
+
+<div>
+  <form method="post">
+    <input type="submit" name="submit" class="btn btn-primary" value="Get patients">
+  </form>
 </div>
 
 
