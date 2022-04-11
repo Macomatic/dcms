2bbb53e85e037fa0ac61e7f11feccce69f09ffc7
AvaneeshM <55166169+AvaneeshM@users.noreply.github.com>
2022-04-11 15:07:48 -0400

    Add Dental Staff Page
    Not done just to help with login redirect

---
+++ dentalStaff.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>DCMS </title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Dental Staff</h2>
+  <form method="post">
+  
+  <button type="records" name="records" class="btn btn-primary">Access Patient Records</button>
+
+</div>
+
+  </form>
+</div>
+
+
+</body>
+</html>
+ 
