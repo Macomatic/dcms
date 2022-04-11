66f48649f1a32c058e9b5133d6c2f778655b9fe9
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2022-04-11 17:14:49 -0400

    Updated Patient Medical History Page

--- patientAppointments.php
+++ patientAppointments.php
@@ -1,7 +1,33 @@
+<?php
+// Include config file
+require_once "config.php";
+
+$patientid = [];
+$nameArray = [];
+$apptdate = [];
+$dentistName = [];
+$treatmentType = [];
+$isCompleted = [];
+
+$query = 'select * from dcms.Patient';
+$rs = pg_query($dbconnect, $query) or die ("Error: ".pg_last_error());
+while ($row = pg_fetch_row($rs)) {
+    $patientid[] = $row[0];
+    $nameArray[] = $row[1];
+}
+
+
+
+?>
 <html>
-    <title>Patient Appointments</title>
-    <h1 style="text-align: center">Patient Appointments</h1>
-        <body>
-            
-        </body>
+    <title>Patient Upcoming Appointments</title>
+    <h1 style="text-align: center">Patient Upcoming Appointments</h1>
+    <div>
+        <?php
+            $length = sizeof($nameArray);
+            for ($i = 0; $i < $length; $i++){
+                echo "<a style='text-align: center'><h3>Patient ID: $patientid[$i] <br> Patient Name: $nameArray[$i]<br></h3>";
+            }
+        ?>
+    </div>
 </html>
--- patientMedicalHistory.php
+++ patientMedicalHistory.php
@@ -2,118 +2,32 @@
 // Include config file
 require_once "config.php";
 
-$fullName = $_POST['fName'].$_POST['mName'].' '.$_POST['lName'];
-$fullAddress = $_POST['st'].', '.$_POST['city'].', '.$_POST['prov'];
-$randomNum = random_int(1,99999999);
-$userName = $fullName.$randomNum;
-$password = "password";
+$patientid = [];
+$nameArray = [];
+$apptdate = [];
+$dentistName = [];
+$treatmentType = [];
+$isCompleted = [];
 
-if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-    $sql = "insert into dcms.\"User\"(user_ID,username,password) values('".$randomNum."','".$userName."','".$password."')";
-    $ret = pg_query($dbconnect, $sql);
-    if($ret) {
-        
-      $sql = "insert into dcms.Patient(patient_ID,name,gender,insurance,ssn,email,dateOfBirth,address) values('".$randomNum."','".$fullName."','".$_POST['gender']."','".$_POST['insurance']."','".$_POST['ssn']."','".$_POST['email']."','".$_POST['dob']."','".$fullAddress."')";
-      $ret = pg_query($dbconnect, $sql);
-      if($ret) {
-        echo "<p style='color:#39C16E;font-weight: bold;'>"."Data saved Successfully. Returning to main reception screen..."."</p>";
-        sleep(2);
-        header('Location: receptionist.php');
-      }
-      else { 
-        echo "Something Went Wrong";
-      }
-
-    }
-    else { 
-        echo "Something Went Wrong";
-    }
-
-    
-    
+$query = 'select * from dcms.Patient';
+$rs = pg_query($dbconnect, $query) or die ("Error: ".pg_last_error());
+while ($row = pg_fetch_row($rs)) {
+    $patientid[] = $row[0];
+    $nameArray[] = $row[1];
 }
-?>
 
+
+
+?>
 <html>
     <title>Patient Medical History</title>
     <h1 style="text-align: center">Patient Medical History</h1>
-        <body>
-        <div class="container">
-        <h2>Add a new patient</h2>
-        <form method="post">
-        
-            <h3>Patient Name</h3>
-            <div class="form-group">
-            <label for="fName">First Name:</label>
-            <input type="text" class="form-control" id="fName" placeholder="Enter first name" name="fName">
-            </div>
-
-            <div class="form-group">
-            <label for="mName">Middle Name (if applicable):</label>
-            <input type="text" class="form-control" id="mName" placeholder="Enter midle name if applicable" name="mName" value="">
-            </div>
-
-            <div class="form-group">
-            <label for="lName">Last Name:</label>
-            <input type="text" class="form-control" id="lName" placeholder="Enter last name" name="lName">
-            </div>
-            
-            <br>
-            <h3>Address</h3>
-            <div class="form-group">
-            <label for="st">Street # and name:</label>
-            <input type="text" class="form-control" id="st" placeholder="Enter street number and name" name="st">
-            </div>
-
-            <div class="form-group">
-            <label for="city">City:</label>
-            <input type="text" class="form-control" id="city" placeholder="Enter city" name="city">
-            </div>
-
-            <div class="form-group">
-            <label for="prov">Province:</label>
-            <input type="text" class="form-control" id="prov" placeholder="Enter province" name="prov">
-            </div>
-            
-            
-            <br>
-            <h3>Personal Info</h3>
-            <div class="form-group">
-            <label for="dob">Date of Birth:</label>
-            <input type="date" class="form-control" id="dob" placeholder="mm / dd / yyyy" name="dob">
-            </div>
-
-            <div class="form-group">
-            <label for="email">Email Address:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
-            </div>
-
-            <div class="form-group">
-            <label for="phoneNum">Phone Number:</label>
-            <input type="number" class="form-control" id="phoneNum" placeholder="Enter phone number" name="phoneNum">
-            </div>
-
-            <div class="form-group">
-            <label for="gender">Gender:</label>
-            <select name="gender" id="gender" class="form-control">
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
-            </select>
-            </div>
-
-            <div class="form-group">
-            <label for="insurance">Insurance:</label>
-            <input type="text" class="form-control" id="insurance" placeholder="Enter insurance" name="insurance">
-            </div>
-
-            <div class="form-group">
-            <label for="ssn">SSN:</label>
-            <input type="number" class="form-control" id="ssn" placeholder="Enter SSN" name="ssn">
-            </div>
-
-            <input type="submit" name="submit" class="btn btn-primary" value="Submit">
-        </form>
-        </div>
-        </body>
+    <div>
+        <?php
+            $length = sizeof($nameArray);
+            for ($i = 0; $i < $length; $i++){
+                echo "<a style='text-align: center'><h3>Patient ID: $patientid[$i] <br> Patient Name: $nameArray[$i]<br></h3>";
+            }
+        ?>
+    </div>
 </html>
