66308cfd7f2755df375d1c625dd1f10f10d3a3ce
Hong Yue Wang <hongyuew26@gmail.com>
2022-04-14 14:32:18 -0400

    Add patient fields to register page

--- register.php
+++ register.php
@@ -2,29 +2,25 @@
 // Include config file
 require_once "config.php";
  
-$randomNum = random_int(1,99999999);
-$address = "123 Chapel St"; // temp address, needs to be set to form value
-$employmentType = "fullTime"; //values should be fullTime or partTime, default
-$ssn = "123123123"; // needs to be filled out by staff
-$salary = 12345; //default values
+$randomNum = strval(random_int(1,99999999));
 
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     
-  $sql = "insert into dcms.\"User\"(user_ID,username,password,role)values('".$randomNum."','".$_POST['username']."','".md5($_POST['pwd']). "','". $_POST['role']."')";
+  $sql = "insert into dcms.\"User\"(user_ID,username,password,role)values(".$randomNum.",'".$_POST['username']."','".md5($_POST['pwd']). "','patient')";
   $ret = pg_query($dbconnect, $sql);
   if($ret){
-      if ($_POST['role'] == 'dentistHygienist' || $_POST['role'] == 'receptionist') { 
-        $sql = "insert into dcms.employee(employee_ID,name,address,role,employmenttype,ssn,salary) values ('".$randomNum."','".$_POST['username']."','".$address."','".$_POST['role']."','".$employmentType."','".$ssn."','".$salary."')"; // setting 'name' to 'username' temporarily since we do not have a name field in register yet
-                                                                                                                                                                                                                                    // address, employmenttype, ssn, salary doesnt have a field in 'register' yet, giving temp values for now
+        $sql = "insert into dcms.patient(patient_ID,name,gender,insurance,ssn,email,dateOfBirth,address,phoneNumber) values (".$randomNum.",'".$_POST['firstname']." ".$_POST['lastname']."','".$_POST['gender']."','".$_POST['insurance']."',".$_POST['ssn'].",'".$_POST['email']."','".$_POST['dateOfBirth']."','".$_POST['address']."','".$_POST['phoneNumber']."')";
+
         $ret = pg_query($dbconnect, $sql);
         if ($ret) {
           echo "User registered Successfully";
         }
 
         else {
+          $sql = "delete from dcms.\"User\" where username = '".$_POST['username']."'";
+          $ret = pg_query($dbconnect, $sql);
           echo "Something Went Wrong";
         }
-      }
           
   }else{
       
@@ -44,28 +40,65 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
 <body>
 
 <div class="container">
-  <h2>Register</h2>
+  <h2>Patient Register</h2>
   <form method="post">
-  
-     
+
     <div class="form-group">
       <label for="username">Username:</label>
       <input type="text" class="form-control" id="username" placeholder="Enter username" name="username">
     </div>
     
-     
     <div class="form-group">
       <label for="pwd">Password:</label>
       <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
     </div>
 
     <div class="form-group">
-      <label for="role">Role:</label>
-      <select class="form-control" name="role">
-        <option value="receptionist">Receptionist</option>
-        <option value="dentistHygienist">Dentist/Hygienist</option>
-        <option value="patient">Patient</option>
+      <label for="firstname">First Name:</label>
+      <input type="text" class="form-control" id="firstname" placeholder="Enter first name" name="firstname">
+    </div>
+
+    <div class="form-group">
+      <label for="lastname">Last Name:</label>
+      <input type="text" class="form-control" id="lastname" placeholder="Enter last name" name="lastname">
+    </div>
+
+    <div class="form-group">
+      <label for="gender">Gender:</label>
+      <select class="form-control" name="gender">
+        <option value="male">Male</option>
+        <option value="female">Female</option>
       </select>
+    </div>
+
+    <div class="form-group">
+      <label for="insurance">Insurance:</label>
+      <input type="text" class="form-control" id="insurance" placeholder="Enter insurance" name="insurance">
+    </div>
+
+    <div class="form-group">
+      <label for="ssn">Social Security Number:</label>
+      <input type="number" class="form-control" id="ssn" placeholder="Enter SSN" name="ssn">
+    </div>
+    
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+
+    <div class="form-group">
+      <label for="dateOfBirth">Date of Birth:</label>
+      <input type="date" class="form-control" id="dateOfBirth" placeholder="YYYY-MM-DD" name="dateOfBirth">
+    </div>
+
+    <div class="form-group">
+      <label for="address">Address:</label>
+      <input type="text" class="form-control" id="address" placeholder="Enter address" name="address">
+    </div>
+
+    <div class="form-group">
+      <label for="phoneNumber">Phone Number:</label>
+      <input type="text" class="form-control" id="phoneNumber" placeholder="0000000000" name="phoneNumber" maxlength="10">
     </div>
      
     <input type="submit" name="submit" class="btn btn-primary" value="Submit">
