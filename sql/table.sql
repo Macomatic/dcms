a02444ea2bc9bdc89b6f9459b7a2da804c982c39
Adhish Maheswaran <amahe023@uottawa.ca>
2022-04-15 11:02:21 -0400

    added Insert scripts to table.sql

--- sql/table.sql
+++ sql/table.sql
@@ -26,7 +26,6 @@ CREATE TABLE Branch(
     professionalismScore INTEGER,
     cleanlinessScore INTEGER,
     communicationScore INTEGER,
-    totalReviews INTEGER,
     PRIMARY KEY (branch_ID)
 );
 
@@ -46,6 +45,7 @@ CREATE TABLE Employee(
 
 CREATE TABLE BranchManager(
 	bManager_ID INTEGER REFERENCES "User"(user_ID),
+    PRIMARY KEY (bManager_ID)
 	branch_ID INTEGER REFERENCES Branch(branch_ID),
 	name VARCHAR(50),
 	FOREIGN KEY (bManager_ID) REFERENCES "User"(user_ID),
@@ -116,3 +116,42 @@ CREATE TABLE Reviews(
     FOREIGN KEY (patient_ID) REFERENCES Patient(patient_ID),
     FOREIGN KEY (branch_ID) REFERENCES Branch(branch_ID)
 );
+INSERT INTO "User"(
+	user_id, username, password, role)
+	VALUES (123456, 'John123', 'randompassword', 'branchmanager');
+INSERT INTO "User"(
+	user_id, username, password, role)
+	VALUES (123457, 'Sam456', 'randomchars', 'employee');
+INSERT INTO "User"(
+	user_id, username, password, role)
+	VALUES (123458, 'Alexa789', 'randomnums', 'patient');
+INSERT INTO patient(
+	patient_id, name, gender, insurance, ssn, email, dateofbirth, address, phonenumber)
+	VALUES (123458, 'Alexa M.', 'female', 'SunLife', 123123123, 'alexa@yahoo.ca', '1999-09-13', '123 Fake St.', '1231234567');
+INSERT INTO branch(
+	branch_id, address, professionalismscore, cleanlinessscore, communicationscore)
+	VALUES (12, '12 Branch st.', 5, 5, 5);
+INSERT INTO employee(
+	employee_id, name, address, role, employmenttype, ssn, salary, branch_id)
+	VALUES (123457, 'Sam W.', '12 Employee St.', 'Dentist', 'Full-Time', 241543234, 90000, 12);
+INSERT INTO branchmanager(
+	bmanager_id, branch_id, name)
+	VALUES (123456, 12, 'John Doe');
+INSERT INTO appointment(
+	appointment_id, treatment_id, patient_id, dentist_id, date, starttime, endtime, appointmenttype, status, room)
+	VALUES (1,1,123458,123457,'2022-04-21','9:00','10:00','cleaning', 'healthy', '314');
+INSERT INTO appointmentprocedure(
+	patient_id, date, procedurecode, proceduretype, description, involvedtooth, procedureamount)
+	VALUES (123458, '2022-04-19', 313, 'cleaning', 'semi-annual general check up', 'all', 300);
+INSERT INTO treatment(
+	treatment_id, patientcondition, treatmenttype, medication, treatmenttype_id)
+	VALUES (1, 'healthy', 'check-up advice','no medication supplied with cleaning amenities', 1);
+INSERT INTO treatmentsymptoms(
+	treatment_id, symptom)
+	VALUES (1, 'no symptoms from treatment');
+INSERT INTO invoice(
+	dateofissue, contactinfo, patientinsurance, amount)
+	VALUES ('2022-04-23', 'branch12dentist@gmail.com', 'SunLife, checkup is covered semi-annually', 0);
+INSERT INTO reviews(
+	review_id, patient_id, branch_id, professionalism, communication, cleanliness, value)
+	VALUES (1, 123458, 12, 'well experienced and knowledgeable', 'concise and clear', 'spotless, no dust or dirt', 5);
